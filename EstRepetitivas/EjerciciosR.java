6dd0f66a9a46ca93daf8f9b4873ad99e7b8a3fa1
davidmp <mamanipari@gmail.com>
2022-05-12 14:59:18 +0000

    imprimit nombre en vertical

--- EstRepetitivas/EjerciciosR.java
+++ EstRepetitivas/EjerciciosR.java
@@ -1,5 +1,7 @@
 package EstRepetitivas;
 
+import java.util.Scanner;
+
 public class EjerciciosR {
 
     public static void imprimirN1_10While() {
@@ -23,14 +25,29 @@ public class EjerciciosR {
         }
     }
 
+    public static void imprimirNombreVertical() {
+        //Declarar Varibales
+        String nombre;
+        //Datos de entrada
+        System.out.println("Ingrese su nombre:");
+        Scanner lt=new Scanner(System.in);
+        nombre=lt.next();
+        //Proceso y Salida
+        for (int i = 0; i < nombre.length(); i++) {
+            System.out.println(nombre.charAt(i));
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("While");
+       /* System.out.println("While");
         imprimirN1_10While();
         System.out.println("Do While");
         imprimirN1_10dowhile();
         System.out.println();
         System.out.println("For:");
-        imprimirN1_10for();
+        imprimirN1_10for(); */
+        imprimirNombreVertical();
+
     }
 
 }
