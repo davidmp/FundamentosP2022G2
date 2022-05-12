05b40c4d424183df6821a3883615b81dac46b689
davidmp <mamanipari@gmail.com>
2022-05-12 14:37:23 +0000

    ejemplo 3 tipos de est. repetitivas

--- EstRepetitivas/EjerciciosR.java
+++ EstRepetitivas/EjerciciosR.java
@@ -8,17 +8,29 @@ public class EjerciciosR {
             System.out.println(numInit);
             numInit=numInit+1;
         }  
-
+    }
+    public static void imprimirN1_10dowhile() {
+        int numInit=1;
+        do {
+            System.out.println(numInit);
+            numInit=numInit+1;
+        } while (numInit<=10);
     }
 
-    public static void imprimirN1_10dowhile() {
-        do {
-            
-        } while (condition);
+    public static void imprimirN1_10for() {
+        for (int numInit = 1; numInit <=10; numInit++) {
+            System.out.println(numInit);
+        }
     }
 
     public static void main(String[] args) {
+        System.out.println("While");
         imprimirN1_10While();
+        System.out.println("Do While");
+        imprimirN1_10dowhile();
+        System.out.println();
+        System.out.println("For:");
+        imprimirN1_10for();
     }
 
 }
