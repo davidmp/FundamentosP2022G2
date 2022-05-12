1d310b9447f300892bf6a2264e5d367e02ac05b7
davidmp <mamanipari@gmail.com>
2022-05-12 14:11:27 +0000

    Ejemplos repetitivos

---
+++ EstRepetitivas/EjerciciosR.java
@@ -0,0 +1,24 @@
+package EstRepetitivas;
+
+public class EjerciciosR {
+
+    public static void imprimirN1_10While() {
+        int numInit=1;
+        while (numInit<=10) {
+            System.out.println(numInit);
+            numInit=numInit+1;
+        }  
+
+    }
+
+    public static void imprimirN1_10dowhile() {
+        do {
+            
+        } while (condition);
+    }
+
+    public static void main(String[] args) {
+        imprimirN1_10While();
+    }
+
+}
