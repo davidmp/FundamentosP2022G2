52c63911a6ba4897acad0d79419b4db88130f9ab
davidmp <mamanipari@gmail.com>
2022-05-12 13:34:27 +0000

    ejemplo examen p3

--- EstRepetitivas/EjerciciosRepetitivas.java
+++ EstRepetitivas/EjerciciosRepetitivas.java
@@ -1,7 +1,11 @@
 package EstRepetitivas;
 
+import java.util.Scanner;
+
 public class EjerciciosRepetitivas {
     
+   static Scanner lt=new Scanner(System.in);
+
     public static void ejer41While() {
         int contador=1;
         int suma=0;
@@ -14,8 +18,19 @@ public class EjerciciosRepetitivas {
         System.out.println("La respuesta es:"+suma);
     }
 
+
+    public static void pregunta2DMP() {
+        System.out.println("Ingrese el operador:");
+        char operador=lt.next().charAt(0);
+        switch (operador) {
+            case '+': System.out.println("debes sumar"); break;
+            case '%': System.out.println("debes sacar el modulo"); break;        
+            default: break;
+        }
+    }
+
     public static void main(String[] args) {
-       ejer41While(); 
+       pregunta2DMP(); 
     }
 
 
