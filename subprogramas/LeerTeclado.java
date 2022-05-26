91f0b8844d280bf762b66db29b9ede3dde1b666b
davidmp <mamanipari@gmail.com>
2022-05-26 15:02:52 +0000

    leer teclado p1

--- subprogramas/LeerTeclado.java
+++ subprogramas/LeerTeclado.java
@@ -1,9 +1,20 @@
 package subprogramas;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class LeerTeclado {
     
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Ya tenemos el "lector"
+
     public int print(String mensaje, int valor) {
-        return 0;
+        System.out.println(mensaje+":");
+        try {
+            valor=Integer.parseInt(br.readLine());
+        } catch (Exception e) {            
+            System.err.println("el valor que ingreso es incorrecto");            
+        }
+        return valor;
     }
 
     public String print(String mensaje, String valor) {
