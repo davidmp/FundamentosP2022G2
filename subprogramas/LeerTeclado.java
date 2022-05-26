616acb1afe0420ae56e30ffd07280290b02a5618
davidmp <mamanipari@gmail.com>
2022-05-26 14:47:55 +0000

    leer teclado

---
+++ subprogramas/LeerTeclado.java
@@ -0,0 +1,24 @@
+package subprogramas;
+
+public class LeerTeclado {
+    
+    public int print(String mensaje, int valor) {
+        return 0;
+    }
+
+    public String print(String mensaje, String valor) {
+        return "0";
+    }
+
+    public float print(String mensaje, float valor) {
+        return 0;
+    }  
+    
+    public double print(String mensaje, double valor) {
+        return 0;
+    }    
+    
+    public boolean print(String mensaje, boolean valor) {
+        return true;
+    }     
+}
