dce928980055a6cec04f316e760206b299c1aeb4
AngelDX <angel.condori@gmail.com>
2022-06-16 09:14:19 -0500

    suma matrices

--- ProyectoGUI/src/main/java/pe/edu/upeu/arrays/MatricesEjemplo.java
+++ ProyectoGUI/src/main/java/pe/edu/upeu/arrays/MatricesEjemplo.java
@@ -21,8 +21,38 @@ public void matricesBasicos() {
        +prod[i][2]+"\t"+prod[i][3]); 
     }
 }
+
+public void sumaMatrices() {
+    int[][] mA={{3,-8},{5,9}};
+    int[][] mB={{-9,11},{1,-5}};
+    int[][] mR=new int[mA.length][mA[0].length];
+    for (int i = 0; i < mA.length; i++) {
+        for (int j = 0; j < mA[0].length; j++) {
+            mR[i][j]=mA[i][j]+mB[i][j];
+        }
+    }
+    imprimirMatrix(mR);
+}
+
+public void imprimirMatrix(int[][] m) {
+    for (int i = 0; i < m.length; i++) {
+        for (int j = 0; j < m[0].length; j++) {
+            System.out.print(m[i][j]+"\t");
+        }
+        System.out.println("");
+    }
+}
+public void imprimirMatrix(Object[][] m) {
+    for (int i = 0; i < m.length; i++) {
+        for (int j = 0; j < m[0].length; j++) {
+            System.out.print(m[i][j]+"\t");
+        }
+        System.out.println("");
+    }
+}
 public static void main(String[] args) {
-    new MatricesEjemplo().matricesBasicos();
+    //new MatricesEjemplo().matricesBasicos();
+    new MatricesEjemplo().sumaMatrices();
 }
 
 }
