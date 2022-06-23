a6eb7e75e0dfe9ca6b163a1e71efb7933e2a4fa6
AngelDX <angel.condori@gmail.com>
2022-06-23 13:57:24 -0500

    Menu de opciones

--- ProyectoGUI/src/main/java/pe/edu/upeu/app/MenuOpciones.java
+++ ProyectoGUI/src/main/java/pe/edu/upeu/app/MenuOpciones.java
@@ -6,28 +6,45 @@ import pe.edu.upeu.modelo.UsuarioTO;
 import pe.edu.upeu.util.LeerTeclado;
 
 public class MenuOpciones {
-    LeerTeclado leerT=new LeerTeclado();
-    UsuarioTO uTo=new UsuarioTO();
-    Console cons=System.console();
-    
+    LeerTeclado leerT = new LeerTeclado();
+    UsuarioTO uTo = new UsuarioTO();
+    Console cons = System.console();
+
     public void login() {
         System.out.println("************Bienvenidos al Sistema de Ventas*************");
-        System.out.println("****************Valide suss credenciales***************");
-        uTo.usuario=leerT.leer("", "Ingrese su usuario");
+        System.out.println("****************Valide sus credenciales***************");
+        uTo.usuario = leerT.leer("", "Ingrese su usuario");
         System.out.println("Ingrese su clave:");
-        char[] clavex=cons.readPassword();        
-        uTo.clave=String.valueOf(clavex);
-        
+        char[] clavex = cons.readPassword();
+        uTo.clave = String.valueOf(clavex);
+
         if (uTo.usuario.equals("davidmp") && uTo.clave.equals("123456")) {
             menuPrincipal();
-        }else{
+        } else {
             System.out.println("Intente Nuevamente!! sus credenciales son invalidos!");
             login();
         }
-    } 
+    }
 
     public void menuPrincipal() {
-        
+        System.out.println("------------------------------------Sistema de Ventas------------------------------");
+        int opciones = 1;
+        String menu = "";
+        menu += "1=Registro de Productos\n";
+        menu += "2=Registro de Ventas\n";
+        System.out.println(menu);
+        do {
+            switch (opciones) {
+                case 1: System.out.println("Registro Productos");   break;
+
+                default: System.out.println("Opcion Invalida!"); break;
+            }
+            String continuar=leerT.leer("",
+             "Desea Continuar? S=Si/N=No").charAt(0)=='S'?"S":"N";
+            if (continuar.charAt(0)=='S') {
+                opciones=leerT.leer(0, "Eliga una operacion:\n"+menu);
+            }else{opciones=0;}
+        } while (opciones != 0);
     }
 
 }
