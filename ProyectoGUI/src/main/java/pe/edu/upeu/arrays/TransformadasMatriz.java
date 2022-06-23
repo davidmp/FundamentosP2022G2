5b575de684dd52aed69bb3de2377cea5b468e0ca
AngelDX <angel.condori@gmail.com>
2022-06-23 15:02:12 -0500

    Transformada 12

--- ProyectoGUI/src/main/java/pe/edu/upeu/arrays/TransformadasMatriz.java
+++ ProyectoGUI/src/main/java/pe/edu/upeu/arrays/TransformadasMatriz.java
@@ -21,16 +21,35 @@ public class TransformadasMatriz {
         imprimirMatrices(matriz);
     }
 
+    public void transformada12() {
+        System.out.println("Ingrese Dimension Matriz:");
+        int dimension=cs.nextInt();
+        System.out.println("Ingrese valor de Inicio:");
+        int valorInit=cs.nextInt();  
+        String[][] matriz=new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = matriz[0].length-1; c>=(0+f) ; c--) {
+                matriz[f][c]=String.valueOf(valorInit);
+                valorInit++;//1
+            }
+        } 
+        imprimirMatrices(matriz);
+    }
     public void imprimirMatrices(String[][] m) {
         for (int f = 0; f < m.length; f++) {
             for (int c = 0; c < m[0].length; c++) {
-                System.out.print(m[f][c]+"\t");               
+                if (m[f][c]==null) {
+                    System.out.print("\t");
+                }else{
+                    System.out.print(m[f][c]+"\t");   
+                }                            
             }
             System.out.println("");
         }
     }
 
     public static void main(String[] args) {
-        new TransformadasMatriz().transformada01();
+       // new TransformadasMatriz().transformada01();
+       new TransformadasMatriz().transformada12();
     }
 }
