abe18ddb7b410ebe4f4ec186a2e6e6d2d1f67de2
AngelDX <angel.condori@gmail.com>
2022-07-05 12:18:09 -0500

    Ventas P3

--- SysVentasG2/src/main/java/pe/edu/upeu/dao/VentaDao.java
+++ SysVentasG2/src/main/java/pe/edu/upeu/dao/VentaDao.java
@@ -20,15 +20,40 @@ public class VentaDao extends AppCrud{
     final static String TABLA_VENTADETALLE="VentaDetalle.txt";
 
     public void registroVentaGeneral() {
-        
+        util.clearConsole();
+        System.out.println("*****************Registro de Ventas***************");
+        vTo=new VentaTO();
+        leerA=new LeerArchivo(TABLA_VENTA);
+        String idVX=generarId(leerA, 0, "V", 1);
+        vTo.setIdVenta(idVX);
+        vTo.setDniruc(validarCliente(leerT.leer("", "Ingrese DNI/RUC cliente")));
+        if (crearVenta(vTo)!=null) {
+            System.out.println("Agregar Productos a carrito de venta");
+        }
+    }
+
+    public VentaTO crearVenta(VentaTO to) {
+        leerA=new LeerArchivo(TABLA_VENTA);
+        agregarContenido(leerA, to);
+        leerA=new LeerArchivo(TABLA_VENTA);
+        Object[][] data=buscarContenido(leerA, 0, to.getIdVenta());
+        if (data.length==1) {
+            return to;
+        }
+        return null;
     }
 
     public void carritoVentas(VentaTO to) {
         
     }
 
-    public void validarCliente(String dniruc) {
-        
+    public String validarCliente(String dniruc) {
+        leerA=new LeerArchivo(TABLA_CLIENTE);
+        Object[][] dataC=buscarContenido(leerA, 0, dniruc);
+        if (dataC==null || dataC.length==0) {
+            new ClienteDao().crearCliente();
+        }
+        return dniruc;
     }
 
     public void mostrarProductos() {
